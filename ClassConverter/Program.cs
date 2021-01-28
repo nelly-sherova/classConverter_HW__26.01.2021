5d9f6239f5d7ef990613cac66926be049962722b
unknown <nellysherova@mail.ru>
2021-01-28 14:23:56 +0300

    Создала класс Converter и его несколько полей и методов

--- ClassConverter/Program.cs
+++ ClassConverter/Program.cs
@@ -2,6 +2,52 @@
 
 namespace ClassConverter
 {
+    class Converter
+    {
+        private double usd;
+        private double eur;
+        private double rub;
+        public double n;
+        public Converter(){}
+        public Converter(double usd, double eur, double rub)
+        {
+            this.usd = usd;
+            this.eur = eur;
+            this.rub = rub;
+        }
+        public double SomToUsd()
+        {
+            return Math.Round((n / usd), 3);
+        }
+        public double UsdToSom()
+        {
+            return Math.Round((n * usd), 3);
+        }
+        public double SomToEur()
+        {
+            return Math.Round((n / eur), 3);
+        }
+        public double EurToSom()
+        {
+            return Math.Round((n * eur), 3);
+        }
+        public double SomToRub()
+        {
+            return Math.Round((n / rub), 3);
+        }
+        public double RubToSom()
+        {
+            return Math.Round((n * rub), 3);
+        }
+        public void currencyExchangeRate()
+        {
+            Console.WriteLine("Курс валют: ");
+            Console.WriteLine($"1 usd => {usd} som");
+            Console.WriteLine($"1 eur => {eur} som");
+            Console.WriteLine($"1 rub => {rub} som");
+
+        }
+    }
     class Program
     {
         static void Main(string[] args)
